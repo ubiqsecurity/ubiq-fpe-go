de458197878f29c21ddee964cb06cc9d632ba649
John Tyner <jtyner@ubiqsecurity.com>
2021-11-29 17:07:41 -0500

    add creation of context structure

---
+++ ffx.go
@@ -0,0 +1,61 @@
+package ubiq
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"errors"
+	"math"
+)
+
+type ffx struct {
+	block cipher.Block
+
+	radix int
+	len   struct {
+		txt, twk struct {
+			min, max int
+		}
+	}
+	twk []byte
+}
+
+func newFFX(key []byte, twk []byte,
+	maxtxt int, maxtwk int, mintwk int,
+	radix int) (*ffx, error) {
+
+	if (radix < 2 || radix > 36) {
+		return nil, errors.New("unsupported radix")
+	}
+
+	mintxt := int(math.Ceil(float64(6) / math.Log10(float64(radix))))
+	if (mintxt < 2 || mintxt > maxtxt) {
+		return nil, errors.New(
+			"unsupported radix/maximum text length combination")
+	}
+
+	if (mintwk > maxtwk || len(twk) < mintwk ||
+		(maxtwk > 0 && len(twk) > maxtwk)) {
+		return nil, errors.New("invalid tweak length")
+	}
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	ctx := new(ffx)
+
+	ctx.block = block
+
+	ctx.radix = radix
+
+	ctx.len.txt.min = mintxt
+	ctx.len.txt.max = maxtxt
+
+	ctx.len.twk.min = mintwk
+	ctx.len.twk.max = maxtwk
+
+	ctx.twk = twk
+
+	return ctx, nil
+}
---
+++ ffx_test.go
@@ -0,0 +1,31 @@
+package ubiq
+
+import (
+	"testing"
+)
+
+func TestNewFFXKeyLength(t *testing.T) {
+	var err error
+	var key [] byte
+
+	var bad_lengths []int = []int{15, 23, 26, 30, 33, 64}
+	var good_lengths []int = []int{16, 24, 32}
+
+	twk := make([]byte, 4)
+
+	for _, len := range bad_lengths {
+		key = make([]byte, len)
+		_, err = newFFX(key, twk, 1024, 0, 0, 10)
+		if err == nil {
+			t.FailNow()
+		}
+	}
+
+	for _, len := range good_lengths {
+		key = make([]byte, len)
+		_, err = newFFX(key, twk, 1024, 0, 0, 10)
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+}
